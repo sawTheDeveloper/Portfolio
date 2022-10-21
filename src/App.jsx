c6f96e0ac2261d02cbfafbfe6abae0a564a752c7
Patrick <sawthedeveloper@gmail.com>
2022-10-21 16:41:11 -0700

    Init

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Services from './components/Services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contacts from './components/Contacts/Contacts'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    < Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portfolio />
+    <Testimonials />
+    <Contacts />
+    <Footer />
+    </>
+  )
+}
+
+export default App
