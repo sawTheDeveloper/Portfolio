df7fd7c6b78a63d5df4d0bc4f33ef840475abb42
Patrick <sawthedeveloper@gmail.com>
2022-10-23 13:12:14 -0700

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div> Header </div>
+      <header>
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Patrick Powell</h1>
+          <h5 className="text-light">Frontend App Developer</h5>
+          <CTA />
+        </div>
+      </header>
   )
 }
 
