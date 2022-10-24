13add11eb30729c1442ba02fa34384c07bfcb406
Patrick <sawthedeveloper@gmail.com>
2022-10-24 11:56:14 -0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Experience from './components/Experience/Experience'
 import Services from './components/Services/Services'
 import Portfolio from './components/Portfolio/Portfolio'
 import Testimonials from './components/Testimonials/Testimonials'
-import Contacts from './components/Contacts/Contacts'
+import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
@@ -19,7 +19,7 @@ const App = () => {
     <Services />
     <Portfolio />
     <Testimonials />
-    <Contacts />
+    <Contact />
     <Footer />
     </>
   ) 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import './Nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
+import {FaHome} from 'react-icons/fa'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TiContacts} from 'react-icons/ti'
+import {AiOutlineUser} from 'react-icons/ai'
+import { useState } from 'react'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <Nav>
+    <nav>
       
-      <a href="#"> <AiOutlineHome/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} ><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <BiBook/></a>
+      <a href="#services"onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}> <RiServiceLine/></a>
+      <a href="#contact"onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <TiContacts/></a>
+      
     
-    </Nav>
+    </nav>
   )
 }
 
