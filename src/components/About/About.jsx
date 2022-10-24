3d5d039d5dea21085947124004b076cefd82062d
Patrick <sawthedeveloper@gmail.com>
2022-10-24 13:28:15 -0700

    update No about me pic+

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './About.css'
 import {ME} from '../../assets/profile-pic.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
@@ -17,13 +21,36 @@ const About = () => {
       Compiled with problems:
       ERROR in ./src/components/About/About.jsx 30:17-19
       Can't import the named export 'ME' (imported as 'ME') from default-exporting module (only default export is available) */}
-      <img src={ME} alt="About Me" />
+      <img src={null} alt="About" />
       </div>
       </div>
 <div className="about__content">
+  <div className="about__cards">
 
+    <article className='about__card'>
+      <FaAward className='about__icon' />
+      <h5>Experience</h5>
+      <small>3 years</small>
+    </article>
 
+    <article className='about__card'>
+      <FiUsers className='about__icon' />
+      <h5>Clients</h5>
+      <small>10+</small>
+    </article>
 
+    <article className='about__card'>
+      <VscFolderLibrary className='about__icon' />
+      <h5>Projects</h5>
+      <small>20+ completed</small>
+    </article>
+  </div>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda nostrum molestiae mollitia blanditiis, minima laborum nobis quis debitis? Culpa dolore vitae sed quis ab vel nam laudantium voluptatem blanditiis?
+    </p>
+
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
 
     </div>
