e03947d78283a999593318bae7f9e72e66835a88
sawTheDeveloper <50364116+sawTheDeveloper@users.noreply.github.com>
2023-10-23 20:33:18 -0700

    Image fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,13 +11,13 @@ const Header = () => {
           <h5>Hello I'm</h5>
           <h1>Patrick Powell</h1>
           <h5 className="text-light">Software Developer</h5>
-          <CTA />
-          <HeaderSocials />
-
           <div className="me">
             <LazyLoadImage src='https://ik.imagekit.io/Nexus253/me.png?ik-sdk-version=javascript-1.4.3&updatedAt=1670398908957' width={600} alt="me" loading="lazy" />
           </div>
+          <CTA />
+          <HeaderSocials />
 
+          
           <a href="#contact" className='scroll__down'>Scroll Down</a>
         </div>
       </header>
