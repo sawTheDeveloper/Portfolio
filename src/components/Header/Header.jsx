6e3c60aa36f818b6e12608fb723a40cf21c81e4c
Patrick <sawthedeveloper@gmail.com>
2024-07-16 01:36:18 -0700

    Update cv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,12 +11,17 @@ const Header = () => {
           <h5>Hello I'm</h5>
           <h1>Patrick Powell</h1>
           <h5 className="text-light">Software Developer</h5>
+
+          <div className="me">
+            <LazyLoadImage src='https://ik.imagekit.io/Nexus253/me.png?ik-sdk-version=javascript-1.4.3&updatedAt=1670398908957'  alt="me" loading="lazy" />
+          </div>
+
+
+            
           <CTA />
           <HeaderSocials />
 
-          <div className="me">
-            <LazyLoadImage src='https://ik.imagekit.io/Nexus253/me.png?ik-sdk-version=javascript-1.4.3&updatedAt=1670398908957' width={600} alt="me" loading="lazy" />
-          </div>
+          
 
           <a href="#contact" className='scroll__down'>Scroll Down</a>
         </div>
